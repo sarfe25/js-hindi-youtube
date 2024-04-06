7609eab4cddc6517a4aa7d90d4c70b28afee1f25
sarfe25 <166203713+sarfe25@users.noreply.github.com>
2024-04-06 07:49:17 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "sarfe@2345"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+//accountId ="2"//not allowed
+
+/*
+Prefer not to use Var beacause of of ussue
+in block scope and functional scope 
+*/
+console.log(accountId);
+accountaEmail ="hc@hc.com"
+accountPassword = "1212121212"
+accountCity = "Bengluru"
+
+console.table( [accountId, accountEmail, accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //Treat all js code as newer version
+
+//alert("3+3")//we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Sarfe")//code readiblity is prioritiesed
+
+let name ="Sarfe"
+let age =20
+let isLoggedIn = false
+let state;
+
+//Numbe has rage is 2the power of 53
+//string use as ""
+//boolean = true / false 
+//null = standalone value 
+//undefined = 
+//symbol=unique
+
+
+
+//object=
+console.log(typeof"sarfe")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
